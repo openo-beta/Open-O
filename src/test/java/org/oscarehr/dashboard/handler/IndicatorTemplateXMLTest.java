8822ca0cde615a65a07df9aaef2ad2b43c377b20
LiamStanziani <LiamStanziani1@outlook.com>
2025-09-01 11:37:09 -0400

    Added into failing test class to include table that isnt being created, even though it should be

--- src/test/java/org/oscarehr/dashboard/handler/IndicatorTemplateXMLTest.java
+++ src/test/java/org/oscarehr/dashboard/handler/IndicatorTemplateXMLTest.java
@@ -37,6 +37,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Before;
+import org.oscarehr.common.dao.utils.SchemaUtils;
 import org.oscarehr.dashboard.query.DrillDownAction;
 import org.oscarehr.dashboard.query.RangeInterface;
 import org.oscarehr.dashboard.query.RangeInterface.Limit;
@@ -48,6 +50,11 @@ public class IndicatorTemplateXMLTest {
 
     private static IndicatorTemplateXML indicatorTemplateXML;
     private static Document xmlDocument;
+
+    @Before
+    public void before() throws Exception {
+        SchemaUtils.restoreTable("demographicExt");
+    }
 
     @BeforeClass
     public static void setUpBeforeClass() {
