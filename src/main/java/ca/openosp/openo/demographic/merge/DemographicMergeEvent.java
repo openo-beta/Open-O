cd25fd68d8025c107c502f6ea4ab5183427db3ca
Deval Italiya <deval.italiya@gmail.com>
2026-03-19 16:26:22 -0400

    add DemographicMergeEvent entity and DAO

---
+++ src/main/java/ca/openosp/openo/demographic/merge/DemographicMergeEvent.java
@@ -0,0 +1,144 @@
+//CHECKSTYLE:OFF
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package ca.openosp.openo.demographic.merge;
+
+import ca.openosp.openo.commn.model.AbstractModel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a single merge or unmerge event in the demographic merge audit log.
+ * <p>
+ * One row is inserted per merge or unmerge operation. The unmerge path reads this
+ * table to identify the primary and all secondary demographics to reactivate.
+ *
+ * @since 2026-03-19
+ */
+@Entity
+@Table(name = "demographic_merge_event")
+public class DemographicMergeEvent extends AbstractModel<Integer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "primary_demographic_no")
+    private int primaryDemographicNo;
+
+    @Column(name = "secondary_demographic_no")
+    private String secondaryDemographicNo;
+
+    @Column(name = "merged_demographic_no")
+    private int mergedDemographicNo;
+
+    @Column(name = "event_type")
+    private String eventType;
+
+    @Column(name = "provider_no")
+    private String providerNo;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "event_date")
+    private Date eventDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getPrimaryDemographicNo() {
+        return primaryDemographicNo;
+    }
+
+    public void setPrimaryDemographicNo(int primaryDemographicNo) {
+        this.primaryDemographicNo = primaryDemographicNo;
+    }
+
+    public String getSecondaryDemographicNo() {
+        return secondaryDemographicNo;
+    }
+
+    public void setSecondaryDemographicNo(String secondaryDemographicNo) {
+        this.secondaryDemographicNo = secondaryDemographicNo;
+    }
+
+    /**
+     * Parses the comma-separated secondary demographic IDs into a list of integers.
+     *
+     * @return List&lt;Integer&gt; list of secondary demographic numbers
+     */
+    public List<Integer> getSecondaryDemographicNos() {
+        return Arrays.stream(secondaryDemographicNo.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public int getMergedDemographicNo() {
+        return mergedDemographicNo;
+    }
+
+    public void setMergedDemographicNo(int mergedDemographicNo) {
+        this.mergedDemographicNo = mergedDemographicNo;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getProviderNo() {
+        return providerNo;
+    }
+
+    public void setProviderNo(String providerNo) {
+        this.providerNo = providerNo;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+}
---
+++ src/main/java/ca/openosp/openo/demographic/merge/DemographicMergeEventDao.java
@@ -0,0 +1,44 @@
+//CHECKSTYLE:OFF
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package ca.openosp.openo.demographic.merge;
+
+import ca.openosp.openo.commn.dao.AbstractDao;
+
+/**
+ * DAO interface for {@link DemographicMergeEvent}.
+ *
+ * @since 2026-03-19
+ */
+public interface DemographicMergeEventDao extends AbstractDao<DemographicMergeEvent> {
+
+    /**
+     * Loads the most recent MERGE event for the given merged demographic (C).
+     * Used by the unmerge path to identify the primary and secondary demographics to reactivate.
+     *
+     * @param mergedDemographicNo Integer the demographic_no of the merged record (C)
+     * @return DemographicMergeEvent the latest MERGE event for the given demographic, or null if not found
+     */
+    DemographicMergeEvent findLatestMergeEventByMergedDemographicNo(Integer mergedDemographicNo);
+}
