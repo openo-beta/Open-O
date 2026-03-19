2b0556428e6e2c695c55486537ad01999b17ba82
Deval Italiya <deval.italiya@gmail.com>
2026-03-19 16:26:22 -0400

    add DemographicMergeEvent entity and DAO

---
+++ src/main/java/ca/openosp/openo/demographic/merge/DemographicMergeEventDaoImpl.java
@@ -0,0 +1,59 @@
+//CHECKSTYLE:OFF
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package ca.openosp.openo.demographic.merge;
+
+import ca.openosp.openo.commn.dao.AbstractDaoImpl;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * JPA implementation of {@link DemographicMergeEventDao}.
+ *
+ * @since 2026-03-19
+ */
+@Repository
+public class DemographicMergeEventDaoImpl extends AbstractDaoImpl<DemographicMergeEvent> implements DemographicMergeEventDao {
+
+    public DemographicMergeEventDaoImpl() {
+        super(DemographicMergeEvent.class);
+    }
+
+    @Override
+    public DemographicMergeEvent findLatestMergeEventByMergedDemographicNo(Integer mergedDemographicNo) {
+        Query q = entityManager.createQuery(
+                "select e from DemographicMergeEvent e " +
+                "where e.mergedDemographicNo = ?1 and e.eventType = 'MERGE' " +
+                "order by e.eventDate desc");
+        q.setParameter(1, mergedDemographicNo);
+        q.setMaxResults(1);
+
+        @SuppressWarnings("unchecked")
+        List<DemographicMergeEvent> results = q.getResultList();
+
+        return results.isEmpty() ? null : results.get(0);
+    }
+}
