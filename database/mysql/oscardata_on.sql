c9f6f63b47aedb90e7f28db5eecfd71c5f714ee6
LiamStanziani <LiamStanziani1@outlook.com>
2026-03-24 16:03:53 -0400

    fix: removed trailing white space in some DB entries, removed IGNORE and initial test data to be replaced with the new data added into ctl_billingservice, archived if using migration file to avoid removing anything that uses the same values that are not test data

--- database/mysql/oscardata_on.sql
+++ database/mysql/oscardata_on.sql
@@ -11552,37 +11552,37 @@ INSERT INTO `billingservice` (`service_code`, `description`, `value`, `percentag
 --
 
 INSERT INTO billingservice (service_compositecode,service_code,description,value,percentage,billingservice_date,specialty,region,anaesthesia,termination_date,displaystyle,sliFlag,gstFlag) VALUES
-     ('','_OMA_A003','General Assessment ','253.35','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_A007','Intermediate Assessment ','110.10','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_A110','Periodic oculo-visual assessment (by a General/Family practitioner) ','141.85','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_C01','Patient interview for practice admission ','128.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_A003','General Assessment','253.35','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_A007','Intermediate Assessment','110.10','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_A110','Periodic oculo-visual assessment (by a General/Family practitioner)','141.85','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_C01','Patient interview for practice admission','128.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F01','Form completion for physicals for schools, camps, pre-school, daycare, university/educational institutions','37.25','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F02','Form completion for physicals for pre-employment certification of fitness/fitness clubs or hospital/nursing home employee','49.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F03','Children''s Aid Society (CAS) application for prospective foster parent ','252.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F04','CRA Disability Tax Credit Certificate (form T2201) ','150.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F05','Insurance Certificate OCF- 3 Disability Certificate ','262.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F06','Insurance Certificate OCF-18 Treatment Plan ','278.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F07','Insurance Certificate OCF-23 Treatment Confirmation ','262.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F08','Attending Physician''s Statement ','200.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F09','Insurance Medical Examination (assessment and report) ','200.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F10','Medical Report for a CPP Disability Benefit (SCISP-2519) ','85.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F11','CPP Narrative Medical Report ','150.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F12','Medical certificate employment insurance sickness benefits INS5140 ','52.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F13','Travel cancellation insurance form ','164.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F14','Life insurance death certificate ','50.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F03','Children''s Aid Society (CAS) application for prospective foster parent','252.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F04','CRA Disability Tax Credit Certificate (form T2201)','150.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F05','Insurance Certificate OCF- 3 Disability Certificate','262.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F06','Insurance Certificate OCF-18 Treatment Plan','278.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F07','Insurance Certificate OCF-23 Treatment Confirmation','262.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F08','Attending Physician''s Statement','200.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F09','Insurance Medical Examination (assessment and report)','200.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F10','Medical Report for a CPP Disability Benefit (SCISP-2519)','85.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F11','CPP Narrative Medical Report','150.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F12','Medical certificate employment insurance sickness benefits INS5140','52.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F13','Travel cancellation insurance form','164.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F14','Life insurance death certificate','50.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F15','Insurance Certificate OCF-19 Determination of Catastrophic Impairment','155.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F16','System-Specific or Disease Specific Questionnaire ','125.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F17','System-Specific Examination ','152.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F18','Assessments: Clarification Report ','300.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F16','System-Specific or Disease Specific Questionnaire','125.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F17','System-Specific Examination','152.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F18','Assessments: Clarification Report','300.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F19','Assessments: Full Narrative Report','350.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F20','Assessments: Independent Medical Examination','200.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F21','Terminal Illness Medical Attestation for a Disability Benefit (ISP2530B)','85.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F22','Reassessment Medical Report (ISP2509)','25.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F23','Scannable Impairment Evaluation (IMPAIR)','50.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_F24','Medical Report – Recurrence of the Same Medical Problem (ISP2525)','25.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_F25','Drivers medical examination (form only) ','77.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_G010','Urinalysis – without microscopy ','7.65','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
-     ('','_OMA_N01','Sick notes (includes return to work/school notes) ','26.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_F25','Drivers medical examination (form only)','77.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_G010','Urinalysis – without microscopy','7.65','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
+     ('','_OMA_N01','Sick notes (includes return to work/school notes)','26.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_N02','Fitness to work notes','50.00','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_RECOR','Electronic Transfer of Records','30.00','0.00','2026-03-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0),
      ('','_OMA_RX','Dispensing service fee','20.75','0.00','2026-01-01',NULL,NULL,NULL,'9999-12-31',NULL,0,0);
@@ -12581,10 +12581,6 @@ INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,servic
 INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('General Practice','MFP','B994A','Premium','Group3','A',35);
 INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('Service','TST','A007A',' Description 3','Group3','A',1);
 INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('Service','TST','A008A',' Description 3','Group3','A',2);
-INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('PRIVATE','PRI','A007A',' Group 1 Name','Group1','A',1);
-INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('PRIVATE','PRI','A007A',' Group 2 Name','Group2','A',1);
-INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES ('PRIVATE','PRI','A007A',' Group 3 Name','Group3','A',1);
-
 -- OMA Uninsured Services Fees - PRIVATE form mappings (OMD Conformance PC13.19)
 INSERT INTO ctl_billingservice (servicetype_name,servicetype,service_code,service_group_name,service_group,status,service_order) VALUES
      ('PRIVATE','PRI','_OMA_A003','Assessments','Group2','A',1),
